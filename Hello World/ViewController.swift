6ef236b0f4a6dfe4dda5104b888cc3ea1a3505b2
Alwin <alwin@alwins-mbp.lan>
2018-07-26 21:55:19 -0400

    adding hello world files

--- Hello World/ViewController.swift
+++ Hello World/ViewController.swift
@@ -9,9 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var bgImageView: UIImageView!
+    @IBOutlet weak var logoImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        bgImageView.isHidden = true
+        logoImageView.isHidden = true
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +26,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func welcomeBtnWasPressed(_ sender: Any) {
+        bgImageView.isHidden = false
+        logoImageView.isHidden = false
+    }
+    
+    @IBAction func hideBtn(_ sender: Any) {
+        bgImageView.isHidden = true
+        logoImageView.isHidden = true
+    }
 }
 
